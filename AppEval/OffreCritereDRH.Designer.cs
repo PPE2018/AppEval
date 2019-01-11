764f868aa8d605aceb17cbc85c9ccb27e007604d
Amandine <amande038@gmail.com>
2019-01-11 15:03:21 +0100

    test

---
+++ AppEval/OffreCritereDRH.Designer.cs
@@ -0,0 +1,114 @@
+﻿namespace AppEval
+{
+    partial class OffreCritereDRH
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.Offres = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Modifier = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Supprimer = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.labelNom = new System.Windows.Forms.Label();
+            this.buttonAjouter = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Offres,
+            this.Modifier,
+            this.Supprimer});
+            this.dataGridView1.Location = new System.Drawing.Point(29, 98);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(422, 161);
+            this.dataGridView1.TabIndex = 0;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // Offres
+            // 
+            this.Offres.HeaderText = "Offres";
+            this.Offres.Name = "Offres";
+            this.Offres.ReadOnly = true;
+            // 
+            // Modifier
+            // 
+            this.Modifier.HeaderText = "Modifier";
+            this.Modifier.Name = "Modifier";
+            // 
+            // Supprimer
+            // 
+            this.Supprimer.HeaderText = "Supprimer";
+            this.Supprimer.Name = "Supprimer";
+            // 
+            // labelNom
+            // 
+            this.labelNom.AutoSize = true;
+            this.labelNom.Location = new System.Drawing.Point(332, 30);
+            this.labelNom.Name = "labelNom";
+            this.labelNom.Size = new System.Drawing.Size(30, 13);
+            this.labelNom.TabIndex = 1;
+            this.labelNom.Text = "Offre";
+            this.labelNom.UseWaitCursor = true;
+            this.labelNom.Click += new System.EventHandler(this.labelNom_Click);
+            // 
+            // buttonAjouter
+            // 
+            this.buttonAjouter.Location = new System.Drawing.Point(208, 322);
+            this.buttonAjouter.Name = "buttonAjouter";
+            this.buttonAjouter.Size = new System.Drawing.Size(298, 76);
+            this.buttonAjouter.TabIndex = 2;
+            this.buttonAjouter.Text = "Ajouter un critère";
+            this.buttonAjouter.UseVisualStyleBackColor = true;
+            // 
+            // OffreCritereDRH
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.buttonAjouter);
+            this.Controls.Add(this.labelNom);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "OffreCritereDRH";
+            this.Text = "OffreDRH";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Offres;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Modifier;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Supprimer;
+        private System.Windows.Forms.Label labelNom;
+        private System.Windows.Forms.Button buttonAjouter;
+    }
+}
---
+++ AppEval/OffreCritereDRH.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AppEval
+{
+    public partial class OffreCritereDRH : Form
+    {
+        public OffreCritereDRH()
+        {
+            InitializeComponent();
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void labelNom_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
