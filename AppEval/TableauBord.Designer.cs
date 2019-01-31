1440797659cc1a7e8b738c5254bd996ae1048d70
Amandine <amande038@gmail.com>
2019-01-31 12:17:28 +0100

    modif

--- AppEval/TableauBord.Designer.cs
+++ AppEval/TableauBord.Designer.cs
@@ -33,7 +33,10 @@
             this.dataGridViewTableauBord = new System.Windows.Forms.DataGridView();
             this.buttonFermer = new System.Windows.Forms.Button();
             this.Evaluateur = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Note = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.Note_Moyenne = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Candidat = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.labelNomOffre = new System.Windows.Forms.Label();
             this.groupBoxTableauBord.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridViewTableauBord)).BeginInit();
             this.SuspendLayout();
@@ -41,7 +44,7 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(223, 27);
+            this.label1.Location = new System.Drawing.Point(188, 28);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(85, 13);
             this.label1.TabIndex = 0;
@@ -52,7 +55,7 @@
             this.groupBoxTableauBord.Controls.Add(this.dataGridViewTableauBord);
             this.groupBoxTableauBord.Location = new System.Drawing.Point(51, 83);
             this.groupBoxTableauBord.Name = "groupBoxTableauBord";
-            this.groupBoxTableauBord.Size = new System.Drawing.Size(404, 297);
+            this.groupBoxTableauBord.Size = new System.Drawing.Size(534, 315);
             this.groupBoxTableauBord.TabIndex = 1;
             this.groupBoxTableauBord.TabStop = false;
             this.groupBoxTableauBord.Text = "Tableau";
@@ -62,11 +65,13 @@
             this.dataGridViewTableauBord.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
             this.dataGridViewTableauBord.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
             this.Evaluateur,
-            this.Note_Moyenne});
+            this.Note,
+            this.Note_Moyenne,
+            this.Candidat});
             this.dataGridViewTableauBord.Location = new System.Drawing.Point(27, 19);
             this.dataGridViewTableauBord.Name = "dataGridViewTableauBord";
             this.dataGridViewTableauBord.ReadOnly = true;
-            this.dataGridViewTableauBord.Size = new System.Drawing.Size(348, 252);
+            this.dataGridViewTableauBord.Size = new System.Drawing.Size(443, 255);
             this.dataGridViewTableauBord.TabIndex = 0;
             this.dataGridViewTableauBord.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridViewTableauBord_CellContentClick);
             // 
@@ -82,23 +87,43 @@
             // 
             // Evaluateur
             // 
-            this.Evaluateur.Frozen = true;
             this.Evaluateur.HeaderText = "Evaluateur";
             this.Evaluateur.Name = "Evaluateur";
             this.Evaluateur.ReadOnly = true;
             // 
+            // Note
+            // 
+            this.Note.HeaderText = "Note";
+            this.Note.Name = "Note";
+            this.Note.ReadOnly = true;
+            // 
             // Note_Moyenne
             // 
-            this.Note_Moyenne.Frozen = true;
             this.Note_Moyenne.HeaderText = "Note_Moyenne";
             this.Note_Moyenne.Name = "Note_Moyenne";
             this.Note_Moyenne.ReadOnly = true;
+            // 
+            // Candidat
+            // 
+            this.Candidat.HeaderText = "Candidat";
+            this.Candidat.Name = "Candidat";
+            this.Candidat.ReadOnly = true;
+            // 
+            // labelNomOffre
+            // 
+            this.labelNomOffre.AutoSize = true;
+            this.labelNomOffre.Location = new System.Drawing.Point(340, 28);
+            this.labelNomOffre.Name = "labelNomOffre";
+            this.labelNomOffre.Size = new System.Drawing.Size(72, 13);
+            this.labelNomOffre.TabIndex = 3;
+            this.labelNomOffre.Text = "Nom de l\'offre";
             // 
             // TableauBord
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(493, 464);
+            this.ClientSize = new System.Drawing.Size(619, 456);
+            this.Controls.Add(this.labelNomOffre);
             this.Controls.Add(this.buttonFermer);
             this.Controls.Add(this.groupBoxTableauBord);
             this.Controls.Add(this.label1);
@@ -118,6 +143,9 @@
         private System.Windows.Forms.DataGridView dataGridViewTableauBord;
         private System.Windows.Forms.Button buttonFermer;
         private System.Windows.Forms.DataGridViewTextBoxColumn Evaluateur;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Note;
         private System.Windows.Forms.DataGridViewTextBoxColumn Note_Moyenne;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Candidat;
+        private System.Windows.Forms.Label labelNomOffre;
     }
 }
