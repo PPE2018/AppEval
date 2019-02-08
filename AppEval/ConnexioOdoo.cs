3d3846a26ea2f6ffcb7b6109851bd78564f64495
Amandine <amande038@gmail.com>
2019-02-08 17:43:58 +0100

    test

---
+++ AppEval/ConnexioOdoo.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppEval
+{
+    public static class ConnexioOdoo
+    {
+        public static string ConnectOdoo()
+        {
+            return "Host=localhost;Port=4545;Username=amande038@gmail.com;Password=nougat38780;Database=ppe2";
+        }
+    }
+}
