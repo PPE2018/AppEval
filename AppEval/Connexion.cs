d89f3d7ea2e42bdb4ade9143ee4680f88f3a6c4f
Amandine <amande038@gmail.com>
2019-02-21 17:17:33 +0100

    test

--- AppEval/Connexion.cs
+++ AppEval/Connexion.cs
@@ -10,12 +10,12 @@ namespace AppEval
     {
         public static string Connecter()
         {
-            return "Host=localhost;Username=postgres;Password=;Database=BddAppEval";
+            return "Host=localhost;Port=4747;Username=openpg;Password=;Database=BddAppEval";
         }
 
         public static string ConnectOdoo()
         {
-            return "Host=localhost;Port=4545;Username=openpg;Password=openpgpwd;Database=ekara2";
+            return "Host=localhost;Port=8069;Username=openpg;Password=openpgpwd;Database=ppe";
         }
     }
 }
