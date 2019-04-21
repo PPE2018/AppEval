47a2fe26be400cf24bc1865e3772e714e578b9bc
Amandine <amande038@gmail.com>
2019-04-21 21:19:45 +0200

    mis en commentaire ta connection Beug dans getNoteTotal.

--- AppEval/Connexion.cs
+++ AppEval/Connexion.cs
@@ -10,12 +10,14 @@ namespace AppEval
     {
         public static string Connecter()
         {
-            return "Host=localhost;Port=5432;Username=postgres;Password=;Database=BddAppEval";
+            /*return "Host=localhost;Port=5432;Username=postgres;Password=;Database=BddAppEval";*/
+            return "Host=localhost;Port=4747;Username=openpg;Password=;Database=BddAppEval";
         }
 
         public static string ConnectOdoo()
         {
-            return "Host=localhost;Port=4545;Username=openpg;Password=openpgpwd;Database=ekara2";
+            /*return "Host=localhost;Port=4545;Username=openpg;Password=openpgpwd;Database=ekara2";*/
+            return "Host=localhost;Port=4747;Username=openpg;Password=openpgpwd;Database=ppe2";
         }
     }
 }
